752654c00551009c4d9be32e050144c40177bb2d
NoorMohammed17 <noormuhammads1797@gmail.com>
2023-09-02 11:48:42 +0530

    error code added using class and special middleware function

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
-const express = require('express'); //express here is function
-const bodyParser = require('body-parser'); //middleware for parsing
+const express = require("express"); //express here is function
+const bodyParser = require("body-parser"); //middleware for parsing
 
-const placesRouter = require('./routes/places-routes') //imported from routes folder
+const placesRouter = require("./routes/places-routes"); //imported from routes folder
 
 const app = express();
 
-app.use('/api/places',placesRouter) ; // => /api/places...
+app.use("/api/places", placesRouter); // => /api/places...
 
+app.use((error, req, res, next) => {
+  // this special middleware with 4 arguments runs only when had an error
+  if (res.headerSent) {
+    return next(error);
+  }
+  res.status(error.code || 500);
+  res.json({message:error.message || "A unkonwn error occurred!"})
+});
 
-
-app.listen(5000)
+app.listen(5000);
--- routes/places-routes.js
+++ routes/places-routes.js
@@ -1,5 +1,7 @@
 const express = require("express");
 
+const HttpError = require('../models/http-error');
+
 const router = express.Router();
 
 const DUMMY_PLACES = [
@@ -49,6 +51,10 @@ router.get("/:pid", (req, res, next) => {
   const place = DUMMY_PLACES.find((p) => {
     return p.id === placeId;
   });
+
+  if(!place){
+    throw new HttpError('Could not find a place for the provided id.', 404)
+  }
   res.json({ place: place });
 });
 
@@ -58,6 +64,9 @@ router.get("/user/:uid", (req, res, next) => {
   const place = DUMMY_PLACES.find((p) => {
     return p.creator === userId;
   });
+  if(!place){
+     return next (new HttpError('Could not find a place for the provided user id.', 404))
+  }
   res.json({ creator: place });
 });
 
