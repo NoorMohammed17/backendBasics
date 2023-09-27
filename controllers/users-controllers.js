66b47c82b94758120502042e59be0837870fd65c
NoorMohammed17 <noormuhammads1797@gmail.com>
2023-09-27 14:52:28 +0530

    login route created

--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -1,63 +1,73 @@
-const uuid = require('uuid').v4;
+const HttpError = require("../models/http-error");
+
+const uuid = require("uuid").v4;
 const DUMMY_USERS = [
-    {
-      id: "1",
-      name: "John Doe",
-      email: "johndoe@example.com",
-      password: "password123"
-    },
-    {
-      id: "2",
-      name: "Jane Smith",
-      email: "janesmith@example.com",
-      password: "secret456"
-    },
-    {
-      id: "3",
-      name: "Alice Johnson",
-      email: "alicejohnson@example.com",
-      password: "secure789"
-    },
-    {
-      id: "4",
-      name: "Robert Wilson",
-      email: "robertwilson@example.com",
-      password: "letmein123"
-    },
-    {
-      id: "5",
-      name: "Emily Brown",
-      email: "emilybrown@example.com",
-      password: "myp@ssword"
-    }
-  ]
-  
+  {
+    id: "1",
+    name: "John Doe",
+    email: "johndoe@example.com",
+    password: "password123",
+  },
+  {
+    id: "2",
+    name: "Jane Smith",
+    email: "janesmith@example.com",
+    password: "secret456",
+  },
+  {
+    id: "3",
+    name: "Alice Johnson",
+    email: "alicejohnson@example.com",
+    password: "secure789",
+  },
+  {
+    id: "4",
+    name: "Robert Wilson",
+    email: "robertwilson@example.com",
+    password: "letmein123",
+  },
+  {
+    id: "5",
+    name: "Emily Brown",
+    email: "emilybrown@example.com",
+    password: "myp@ssword",
+  },
+];
 
-
-const getUsers = (req,res,next) => {
-
-    res.json({users:DUMMY_USERS})
-
+const getUsers = (req, res, next) => {
+  res.json({ users: DUMMY_USERS });
 };
 
-const signup = (req,res,next) => {
-    const {name,email,password} = req.body;
-
-    const newUser = {
-        id:uuid(),
-        name,
-        email,
-        password,
-    }
-    DUMMY_USERS.push(newUser);
-    res.status(201).json({message:'New user created.'})
-    
+const signup = (req, res, next) => {
+  const { name, email, password } = req.body;
+  const hasUser = DUMMY_USERS.find((u) => u.email === email);
+  if (hasUser) {
+    throw new HttpError("Could not create user,Email already exists", 422);
+    //422 for invalid user input
+  }
+  const newUser = {
+    id: uuid(),
+    name,
+    email,
+    password,
+  };
+  DUMMY_USERS.push(newUser);
+  res.status(201).json({ createdUser: newUser });
 };
 
-const login = (req,res,next) => {
-    
-}
+const login = (req, res, next) => {
+  const { email, password } = req.body;
+  const identifiedUser = DUMMY_USERS.find((u) => u.email === email);
+  if (!identifiedUser || identifiedUser.password !== password) {
+    throw new HttpError(
+      "Could not identify user, credentials seem to be wrong",
+      401
+    );
+    //401 for unauthorised
+  }
+  res.json({ message: "Logged in!" });
+};
 
 exports.getUsers = getUsers;
 exports.signup = signup;
-exports.login=login;
+exports.login = login;
